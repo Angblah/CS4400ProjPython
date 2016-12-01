51935db1bbea2b5ed49cfa6712eedb91713b499e
Alex Thompson <thompson.ab@hotmail.com>
2016-12-01 16:27:18 -0500

    got rid of merge nastiness

--- static/services.js
+++ static/services.js
@@ -2,12 +2,9 @@ angular.module('myApp').factory('UserService',
 ['$timeout', '$http', function ($timeout, $http) {
 
     var user = null;
-<<<<<<< HEAD
 
-=======
     var isLoggedIn = false;
-    
->>>>>>> origin/master
+
     function authenticate(username, password) {
         //http://twisted.readthedocs.io/en/twisted-16.2.0/core/howto/defer-intro.html
         //See above on deffereds for async code
@@ -36,11 +33,8 @@ angular.module('myApp').factory('UserService',
 
     return ({
         authenticate: authenticate,
-<<<<<<< HEAD
-        register: register
-=======
+        register: register,
         user: user,
         isLoggedIn: isLoggedIn
->>>>>>> origin/master
     });
 }]);
