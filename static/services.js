319c07f4d7f7f114d789d43a917c03cf30780d4d
Angelo Marcelo <Angelo Marcelo>
2016-12-01 15:22:22 -0500

    Added user and isLoggedIn vars to Services

--- static/services.js
+++ static/services.js
@@ -2,6 +2,7 @@ angular.module('myApp').factory('UserService',
 ['$timeout', '$http', function ($timeout, $http) {
 
     var user = null;
+    var isLoggedIn = false;
     
     function authenticate(username, password) {
         //http://twisted.readthedocs.io/en/twisted-16.2.0/core/howto/defer-intro.html
@@ -18,6 +19,8 @@ angular.module('myApp').factory('UserService',
     }
 
     return ({
-        authenticate: authenticate
+        authenticate: authenticate,
+        user: user,
+        isLoggedIn: isLoggedIn
     });
 }]);
