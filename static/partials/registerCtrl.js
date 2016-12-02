0f559a25562fad2e9ac5426aac1c0d15504821d6
Alex Thompson <thompson.ab@hotmail.com>
2016-12-01 21:34:34 -0500

    set up registration validation

--- static/partials/registerCtrl.js
+++ static/partials/registerCtrl.js
@@ -2,8 +2,23 @@ angular.module('myApp').controller('registerController',
   ['$scope', '$location', 'UserService',
   function ($scope, $location, UserService) {
 
-    $scope.register = function () {
+    // $scope.userCheckFail = false;
+    // $scope.passCheckFail = false;
+    // $scope.passMatchFail = false;
+    $scope.registerform = {
+      username: "",
+      email: "",
+      password: "",
+      confpassword: ""
+    };
 
+    $scope.register = function () {
+      if ($scope.validate($scope.registerform.username, $scope.registerform.email, $scope.registerform.password, $scope.registerform.confpassword)) {
+        return;
+      }
+      if (!$scope.registerform.email) {
+        return;
+      }
       // initial values
       $scope.error = false;
       $scope.disabled = true;
@@ -20,12 +35,35 @@ angular.module('myApp').controller('registerController',
             $scope.registerform = {};
           } else {
             $scope.error = true;
-            $scope.errorMessage = "Unable to Register, username or email might already exist";
+            $scope.errorMessage = "Unable to Register, username or email already exists";
             $scope.disabled = false;
             $scope.registerform = {};
           }
         });
       };
 
+    $scope.validate = function (username, email, password, confpassword) {
+      $scope.userCheckFail = false;
+      $scope.emailCheckFail = false;
+      $scope.passCheckFail = false;
+      $scope.passMatchFail = false;
+
+      if (username == "") {
+        $scope.userCheckFail = true;
+        return false;
+      }
+      if (email == "") {
+        $scope.emailCheckFail = true;
+        return false;
+      }
+      if (password == "") {
+        $scope.passCheckFail = true;
+        return false;
+      }
+      if (password != confpassword) {
+        $scope.passMatchFail = true;
+        return false;
+      }
+    }
 
 }])
