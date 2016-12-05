23db8b1dd9acc411f5d582c35c194e3418d570ac
Angelo Marcelo <Angelo Marcelo>
2016-12-04 21:07:13 -0500

    Started View Course and View Project Controllers

---
+++ static/partials/student/js/courseCtrl.js
@@ -0,0 +1,18 @@
+angular.module('myApp').controller('courseController',
+  ['$scope', '$location', 'UserService',
+  function ($scope, $location, UserService) {
+      
+
+      $scope.data = {
+          username: UserService.getUser(),
+          email: "",
+          majorOptions: [{Major_Name: "", Dept_Name: ""}],
+          selectedMajor: {Major_Name: "", Dept_Name: ""},
+          userMajor: {Major_Name: "", Dept_Name: ""},
+          yearOptions: ["Freshman", "Sophomore", "Junior", "Senior"],
+          selectedYear: "",
+          selectedDepartment: ""
+      }
+      
+     
+}])
